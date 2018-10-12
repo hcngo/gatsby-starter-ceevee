5e5643c481d5a283444512b26eac9368ba869489
cngo <Hieu_Ngo@intuit.com>
2018-10-12 15:01:41 -0600

    updated banner with my name

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,7 +4,7 @@ import SocialLinks from "./SocialLinks";
 const Banner = () => (
   <div className="row banner">
     <div className="banner-text">
-      <h1 className="responsive-headline">I'm Jonathan Doe.</h1>
+      <h1 className="responsive-headline">Hieu Ngo</h1>
       <h3>
         I'm a <span>full-stack software developer</span>, specialized in
         <span>web-based application development</span> with working knowledge and experience in modern front-end technologies as well as back-end frameworks.
